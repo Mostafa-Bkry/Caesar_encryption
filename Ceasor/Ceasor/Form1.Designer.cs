7f8c0234e2a7d17a2db8c788f0d900c34bafdf62
MostafaBkry <mbkry661@gmail.com>
2021-11-06 12:12:02 +0200

    edit-ui

--- Ceasor/Ceasor/Form1.Designer.cs
+++ Ceasor/Ceasor/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(163, 265);
+            this.button1.Location = new System.Drawing.Point(182, 267);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(93, 55);
             this.button1.TabIndex = 0;
@@ -55,7 +55,7 @@
             // 
             this.text.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.text.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.text.Location = new System.Drawing.Point(163, 22);
+            this.text.Location = new System.Drawing.Point(182, 24);
             this.text.Name = "text";
             this.text.Size = new System.Drawing.Size(224, 32);
             this.text.TabIndex = 1;
@@ -65,7 +65,7 @@
             // 
             this.key.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.key.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.key.Location = new System.Drawing.Point(240, 78);
+            this.key.Location = new System.Drawing.Point(259, 80);
             this.key.Name = "key";
             this.key.Size = new System.Drawing.Size(72, 32);
             this.key.TabIndex = 1;
@@ -77,7 +77,7 @@
             this.encText.Enabled = false;
             this.encText.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.encText.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.encText.Location = new System.Drawing.Point(163, 136);
+            this.encText.Location = new System.Drawing.Point(182, 138);
             this.encText.Name = "encText";
             this.encText.Size = new System.Drawing.Size(224, 32);
             this.encText.TabIndex = 1;
@@ -88,7 +88,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(110, 26);
+            this.label1.Location = new System.Drawing.Point(129, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(47, 24);
             this.label1.TabIndex = 2;
@@ -99,7 +99,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(192, 82);
+            this.label2.Location = new System.Drawing.Point(211, 84);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(42, 24);
             this.label2.TabIndex = 2;
@@ -110,7 +110,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(57, 140);
+            this.label3.Location = new System.Drawing.Point(76, 142);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(100, 24);
             this.label3.TabIndex = 2;
@@ -120,7 +120,7 @@
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(294, 265);
+            this.button2.Location = new System.Drawing.Point(313, 267);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(93, 55);
             this.button2.TabIndex = 3;
@@ -134,7 +134,7 @@
             this.decText.Enabled = false;
             this.decText.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.decText.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.decText.Location = new System.Drawing.Point(163, 199);
+            this.decText.Location = new System.Drawing.Point(182, 201);
             this.decText.Name = "decText";
             this.decText.Size = new System.Drawing.Size(224, 32);
             this.decText.TabIndex = 1;
@@ -145,7 +145,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(57, 203);
+            this.label4.Location = new System.Drawing.Point(76, 205);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(100, 24);
             this.label4.TabIndex = 2;
